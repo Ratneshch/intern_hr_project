365d131318c8b62a5a826a5925a3d2607b014dbb
Ratnesh Chaurasiya <ratneshchaurasiya7809@gmail.com>
2025-11-12 15:18:34 +0530

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,9 @@ import HeroPage from "../components/HeroPage";
 import Services from '../components/Services';
 import UseCases from '../components/Usecases';
 import TrustedBrands from '../components/TrustedBrand';
+import Reviews from '../components/Reviews';
+import Page2 from '../pages/Page2';
+import ContactPage from '../pages/ContactPage'
 const Home = () => {
   return (
     <div>
@@ -13,6 +16,8 @@ const Home = () => {
       
       <Services />
       <UseCases />
+      <Reviews/>
+      <ContactPage/>
     </div>
   )
 }
