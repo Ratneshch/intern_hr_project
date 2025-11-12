8eea74533b44305066bc0c2f67d6a24b4ca0605c
Ratnesh Chaurasiya <ratneshchaurasiya7809@gmail.com>
2025-11-12 15:02:26 +0530

    new updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,18 @@
 import React from "react";
-import Navbar from "./components/Navbar"; 
+import Navbar from "./components/Navbar";
 
 import "./i18n";
-<<<<<<< HEAD
-import Home from "./pages/Home"; 
-import Page2 from "./pages/Page2";
-import ContactPage from "./pages/ContactPage";
-=======
 import Home from "./pages/Home";
+
 import Footer from "./components/Footer";
->>>>>>> b9c73e2fc2ac6f9bb0ec223cc59416f19ee4d037
 
 const App = () => {
   return (
     <div className="font-[Poppins] relative overflow-hidden">
-    
       <Navbar />
-<<<<<<< HEAD
-<Home/> 
-<Page2/>
-<ContactPage/>
-     
-=======
-      <Home/>
+      <Home />
+
       <Footer />
->>>>>>> b9c73e2fc2ac6f9bb0ec223cc59416f19ee4d037
-      
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,19 @@ import HeroPage from "../components/HeroPage";
 import Services from '../components/Services';
 import UseCases from '../components/Usecases';
 import TrustedBrands from '../components/TrustedBrand';
+import Page2 from "../pages/Page2";
+import ContactPage from "../pages/ContactPage";
 const Home = () => {
   return (
     <div>
       <HeroPage />
+         <Page2 />
       <TrustedBrands />
+    
+      
       <Services />
       <UseCases />
+      <ContactPage />
     </div>
   )
 }
