721524a18677d2de2c886040392c23be171ba33e
Ratnesh Chaurasiya <ratneshchaurasiya7809@gmail.com>
2025-11-12 16:06:18 +0530

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import HeroPage from "../components/HeroPage"; 
 import Services from '../components/Services';
-import UseCases from '../components/Usecases';
 import TrustedBrands from '../components/TrustedBrand';
 import Reviews from '../components/Reviews';
 import Page2 from '../pages/Page2';
